32b90398084e26a3ff36302df5f9eb533ed441a5
qlei <rlei@libertyglobal.com>
2021-02-09 23:19:03 -0800

    restart dhcp server when wan-status notification is received ( OFW-297 )
    The DHCP server lease time needs to be shorter when the WAN interface
    is down. Therefore the DHCP server needs to be restarted when the WAN
    status changes.
    
    See also corresponding changes in utopia to control the lease time
    based on the WAN status.

--- source/gw_prov_sm.c
+++ source/gw_prov_sm.c
@@ -2767,6 +2767,9 @@ static void *GWP_sysevent_threadfunc(void *data)
                 else if (!strcmp(val, "stopped"))
                 { // if WAN down, the zebra need to re-launch to take this change for RA adapting
                     system("service_routed radv-restart");
+                   // dhcp server restart event will regenerate dnsmasq.conf file and restart dnsmasq service.
+                   // DHCP options (lease time, dns_server troubleshoot_wizard(how?) changes) add into dnsmasq.conf file depends on wan_status.             
+                   sysevent_set(sysevent_fd_gs, sysevent_token_gs, "dhcp_server-restart", "1", 0);
                 }
                 // LGI ADD END
 			}
@@ -2879,7 +2882,8 @@ static int GWP_act_DocsisLinkDown_callback_2()
        // LGI ADD - START
        // dhcp server restart event will regenerate dnsmasq.conf file and restart dnsmasq service.
        // DHCP options (lease time, troubleshoot_wizard changes) add into dnsmasq.conf file depends on wan_status.
-       sysevent_set(sysevent_fd_gs, sysevent_token_gs, "dhcp_server-restart", "1", 0);
+       // 20210209: restart dhcp server after wan-status changes to "stopped" in GWP_sysevent_threadfunc().
+       //sysevent_set(sysevent_fd_gs, sysevent_token_gs, "dhcp_server-restart", "1", 0);
        // LGI ADD - END
     }
 
