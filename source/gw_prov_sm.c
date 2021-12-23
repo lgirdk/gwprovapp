bdf8c8e2e851f2973ff95e730e545c5a52e21fc5
mkamat <mkamate@libertyglobal.com>
2021-12-23 14:23:45 -0800

    drop explicit calls to syscfg_init()
    No longer required as syscfg_init() is now handled automatically.

--- source/gw_prov_sm.c
+++ source/gw_prov_sm.c
@@ -3116,7 +3116,6 @@ static int GWP_act_ProvEntry_callback()
     GWPROV_PRINT(" Calling /etc/utopia/utopia_init.sh \n"); 
     v_secure_system("/etc/utopia/utopia_init.sh");
 
-    syscfg_init();
     if (0 != GWP_SysCfgGetInt("bridge_mode"))
     {
         bridgeModeInBootup = 1;
@@ -3139,8 +3138,6 @@ static int GWP_act_ProvEntry_callback()
     v_secure_system("mkdir -p /var/run/firewall");
 
     v_secure_system("/etc/utopia/utopia_init.sh");
-
-    syscfg_init();
 
     sleep(2);
 
