ac2aad0ad2f5f17757e4087f4fbbd1d909c4da2d
Saravanakumar Karthikeyan <skarthikeyan.contractor@libertyglobal.com>
2022-10-17 17:50:19 +0530

    add retry of LanMode update if failed during boot up provisioning ( OFW-3534 F3896SI-1416 )
    fix Modem continues to be in bridge mode after switching to router mode.
    
    BUG DESCRIPTION:
    
    Modem continues to be in bridge mode after switching to router mode.
    
    The system call used to set the value of Device.X_CISCO_COM_DeviceControl.LanManagementEntry.1.LanMode is failing Intermittently at the time of boot-up.
    
    FIX DESCRIPTION:
    
    Added changes to retry to set the dml if it's not succeeded in the first time.
    
    TESTING PERFORMED:
    
    1.Provision the modem with max cpe 2  in bridge mode using bootfile
    2.Reboot from GUI
    3.Provision the modem with max cpe 2 in router mode using bootfile
    4.Reboot from GUI
    5.Device changed to router mode and gpv of Device.X_CISCO_COM_DeviceControl.LanManagementEntry.1.LanMode is showing value as router
    
    Upstream Candidate: May be, On top of TLV202 implementation.
    
    Signed-off-by: Saravanakumar Karthikeyan <skarthikeyan.contractor@libertyglobal.com>

--- source/gw_prov_sm.c
+++ source/gw_prov_sm.c
@@ -305,6 +305,8 @@ static TlvParseCallbackStatusExtIf_e GW_setTopologyMode(Uint8 type, Uint16 lengt
 #endif
 
 /* New implementation !*/
+static int GWP_IssueCmdWithTimeout (char *cmd, char *respStr, int timeout);
+
 static void LAN_start();
 void setGWP_ipv4_event();
 void setGWP_ipv6_event();
@@ -1567,7 +1569,7 @@ static void GWP_EnableERouter(void)
     //v_secure_system("sysevent set bridge_mode 0");
     //v_secure_system("sysevent set forwarding-restart");
 	GWP_EnterRouterMode();
-    v_secure_system("dmcli eRT setv Device.X_CISCO_COM_DeviceControl.LanManagementEntry.1.LanMode string router");
+    GWP_IssueCmdWithTimeout("dmcli eRT setv Device.X_CISCO_COM_DeviceControl.LanManagementEntry.1.LanMode string router", "succeed", 30);
 
     printf("******************************\n");
     printf("* Enabled (after cfg file)  *\n");
@@ -1639,8 +1641,7 @@ static void GWP_DisableERouter(void)
     
     
     GWP_EnterBridgeMode();
-    v_secure_system("dmcli eRT setv Device.X_CISCO_COM_DeviceControl.LanManagementEntry.1.LanMode string bridge-static");
-
+    GWP_IssueCmdWithTimeout("dmcli eRT setv Device.X_CISCO_COM_DeviceControl.LanManagementEntry.1.LanMode string bridge-static", "succeed", 30);
     printf("******************************\n");
     printf("* Disabled (after cfg file)  *\n");
     printf("******************************\n");
