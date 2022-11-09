b18ef28ef67c6e2118aad500018136e9776e2b0d
Andy Carvalho <acarvalho.contractor@libertyglobal.com>
2022-11-09 16:25:39 +0100

    send forwarding-restart signal after LanMode update ( F3896SI-1446 OFW-3577 )
    Fix brlan0 is not getting ipv6 address in dual stack mode.
    
    [BUG DESCRIPTION]
      dibbler-server not running as brlan0 is stuck in partial state.
    
    [FIX DESCRIPTION]
      restart service forwarding after bridge mode is properly updated.
    
    [TESTING PERFORMED]
      Step(1) Perform FR with device mode in dual stack.
      Step(2) Change device mode to modem mode using boot config file and reboot the device.
      Step(3) Change device mode to dual stack mode using boot config file
      Step(4) Reboot the device and verify brlan0 is getting ipv6 address.
    
    Upstream Candidate: No, LG Specific Bug fix.
    
    Signed-off-by: Andy Carvalho <acarvalho.contractor@libertyglobal.com>

--- source/gw_prov_sm.c
+++ source/gw_prov_sm.c
@@ -1559,6 +1559,8 @@ static void GWP_EnableERouter(void)
 	GWP_EnterRouterMode();
     IssueCmdWithTimeout("dmcli eRT setv Device.X_CISCO_COM_DeviceControl.LanManagementEntry.1.LanMode string router", "succeed", 30);
 
+    sysevent_set(sysevent_fd_gs, sysevent_token_gs, "forwarding-restart", "", 0);
+
     printf("******************************\n");
     printf("* Enabled (after cfg file)  *\n");
     printf("******************************\n");
@@ -1594,7 +1596,6 @@ static void GWP_EnterRouterMode(void)
 
     v_secure_system("dmcli eRT setv Device.X_CISCO_COM_DeviceControl.ErouterEnable bool true");
     
-    sysevent_set(sysevent_fd_gs, sysevent_token_gs, "forwarding-restart", "", 0);
 #if defined (_COSA_BCM_ARM_)
     sendPseudoBridgeModeMessage(FALSE);
 #endif
@@ -1631,6 +1632,8 @@ static void GWP_DisableERouter(void)
     GWP_EnterBridgeMode();
     IssueCmdWithTimeout("dmcli eRT setv Device.X_CISCO_COM_DeviceControl.LanManagementEntry.1.LanMode string bridge-static", "succeed", 30);
 
+    sysevent_set(sysevent_fd_gs, sysevent_token_gs, "forwarding-restart", "", 0);
+
     printf("******************************\n");
     printf("* Disabled (after cfg file)  *\n");
     printf("******************************\n");
@@ -1657,7 +1660,6 @@ static void GWP_EnterBridgeMode(void)
     sysevent_set(sysevent_fd_gs, sysevent_token_gs, "bridge_mode", BridgeMode, 0);
     v_secure_system("dmcli eRT setv Device.X_CISCO_COM_DeviceControl.ErouterEnable bool false");
     
-    sysevent_set(sysevent_fd_gs, sysevent_token_gs, "forwarding-restart", "", 0);
 #if defined (_COSA_BCM_ARM_)
     sendPseudoBridgeModeMessage(TRUE);
 #endif
