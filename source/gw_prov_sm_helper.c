c4c8ee70316ca192ece577be08f7a64b95d1b832
Govindaraja <gnb.ext@libertyglobal.com>
2023-08-16 17:00:47 +0530

    fix atom side event markers are not uploading ( OFW-4932 )
    [Description]
    Telemetry will check for this /tmp/cfg_file_applied file to upload the event markers.
    But the /tmp/cfg_file_applied file is missing on the atom side.
    
    [Solution]
    File /tmp/cfg_file_applied is created from arm using rpcclient2.
    
    [Testing]
    1. Make sure the device up and running.
    2. Add atom event markers to the DCM config file.
    3. Enable the telemetry.
    4. check the telemetry upload in ODH server and make sure the atom side event markers are present.
    
    Signed-off-by: Govindaraja N B <gnb.ext@libertyglobal.com>

--- source/gw_prov_sm_helper.c
+++ source/gw_prov_sm_helper.c
@@ -1807,6 +1807,9 @@ static void *GW_DmObjectThread(void *pParam)
             sysevent_set(sysevent_fd_gs, sysevent_token_gs, "cfgfile_status", "End", 0);
             sysevent_set(sysevent_fd_gs, sysevent_token_gs, "cfgfile_apply", "", 0);
             system("touch /tmp/cfg_file_applied");
+#if defined(_PUMA6_ARM_)
+            system("rpcclient2 'touch /tmp/cfg_file_applied' &");
+#endf
             gbDmObjectParseCfgDone = false;
         }
     }
