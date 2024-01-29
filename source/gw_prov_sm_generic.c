91c23dd4db0a63556e66a7a26f994161f3376139
David Fang <david_fang@cable.comcast.com>
2024-01-29 23:52:09 +0000

    XB10-101: XB10:Enable WAN_Manager
    Reason for change: Build errors
    Test Procedure: None
    Risks: None
    
    Change-Id: I43885e732d5e5a03e44154ce72312fbff69d979a
    Signed-off-by: David Fang <david_fang@cable.comcast.com>
    (cherry picked from commit 8101f805dac979fdaf0aaaed108331d19e051a76)
    (cherry picked from commit 7615e490c58eea8452df4e2a78879c62348d55a0)

--- source/gw_prov_sm_generic.c
+++ source/gw_prov_sm_generic.c
@@ -997,10 +997,11 @@ static void *GWP_sysevent_threadfunc(void *data)
                     }
 #endif
 
+#ifndef XB10_ONLY_SUPPORT
                     if(0 != platform_hal_setLed(&ledMgmt)) {
-
                         GWPROV_PRINT("platform_hal_setLed failed\n");
                     }
+#endif
 
                     // Set LED state to RED
                 }
@@ -1097,10 +1098,11 @@ static void *GWP_sysevent_threadfunc(void *data)
                             GWPROV_PRINT("Device is not in Captive Portal, setting LED to SOLID WHITE \n");
                         }
 
+#ifndef XB10_ONLY_SUPPORT
                         if(0 != platform_hal_setLed(&ledMgmt)) {
                             GWPROV_PRINT("platform_hal_setLed failed\n");
-
                         }
+#endif
                     }
                 }
             }
